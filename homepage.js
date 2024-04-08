2cd0889a3878a2cbf5ab852805d6d98921165178
m.hamza hashim <m.hamzahashim1@gmail.com>
2024-04-08 21:25:13 +0500

    completes it

--- homepage.js
+++ homepage.js
@@ -8,7 +8,7 @@ var post = document.getElementById("post");
 
 postBtn.addEventListener("click", function () {
   if (quill.root.innerText.trim() !== '') {
-    post.innerHTML += `<div id="post-card">${quill.root.innerHTML}</div>`;
+    post.innerHTML += `<div id="post-card"><p>${quill.root.innerHTML}<p></div>`;
   }
   postBtn.style.backgroundColor = "#ffac00";
   window.setTimeout(function () {
