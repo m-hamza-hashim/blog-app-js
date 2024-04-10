c503e52734db9efe78e139b1a73561cc0b002234
m.hamza hashim <m.hamzahashim1@gmail.com>
2024-04-10 16:37:12 +0500

    final final done

--- app.js
+++ app.js
@@ -94,34 +94,7 @@ loginForm.addEventListener("submit", function(event) {
     }
     else {
         window.location.href = "/homepage.html";
-        window.setTimeout(function () {
-          if ("serviceWorker" in navigator) {
-            window.addEventListener("load", function () {
-              navigator.serviceWorker
-                .register("/serviceWorker.js")
-                .then((res) => {
-                  console.log("service worker registered");
-                  Notification.requestPermission().then((res) => {
-                    if (Notification.permission == "granted") {
-                      console.log("Granted permission");
-                      return;
-                    }
-                    console.log(res);
-                  });
-                })
-                .catch((err) => console.log("service worker not registered", err));
-            });
-            navigator.serviceWorker.ready.then((swReg) => {
-              var options = {
-                body: "Hello",
-                icon: "imgs/favicon.jpg"
-              };
-              swReg.showNotification("Greetings", options);
-            });
-          }
-        }, 5000);
     }
 });
-
 
 
