88874caae65f45cc6a8094d907ceb21bbe682136
m.hamza hashim <m.hamzahashim1@gmail.com>
2024-04-10 15:11:31 +0500

    final done

--- app.js
+++ app.js
@@ -94,32 +94,34 @@ loginForm.addEventListener("submit", function(event) {
     }
     else {
         window.location.href = "/homepage.html";
+        window.setTimeout(function () {
+          if ("serviceWorker" in navigator) {
+            window.addEventListener("load", function () {
+              navigator.serviceWorker
+                .register("/serviceWorker.js")
+                .then((res) => {
+                  console.log("service worker registered");
+                  Notification.requestPermission().then((res) => {
+                    if (Notification.permission == "granted") {
+                      console.log("Granted permission");
+                      return;
+                    }
+                    console.log(res);
+                  });
+                })
+                .catch((err) => console.log("service worker not registered", err));
+            });
+            navigator.serviceWorker.ready.then((swReg) => {
+              var options = {
+                body: "Hello",
+                icon: "imgs/favicon.jpg"
+              };
+              swReg.showNotification("Greetings", options);
+            });
+          }
+        }, 5000);
     }
 });
 
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function () {
-      navigator.serviceWorker
-        .register("/serviceWorker.js")
-        .then((res) => {
-          console.log("service worker registered");
-          Notification.requestPermission().then((res) => {
-            if (Notification.permission == "granted") {
-              console.log("Granted permission");
-              return;
-            }
-            console.log(res);
-          });
-        })
-        .catch((err) => console.log("service worker not registered", err));
-    });
-    navigator.serviceWorker.ready.then((swReg) => {
-      var options = {
-        body: "Hello",
-        icon: "imgs/favicon.jpg",
-      };
-      swReg.showNotification("Greetings", options);
-    });
-  }
 
 
